7b57031ddb48daa453ae0bdeeaf7382acc50345a
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-08 00:34:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,11 +24,4 @@ class App < Sinatra::Base
     "#{@result}"
   end
 
-
- #  get '/multiply/:num1/:num2' do
- #   @num1 = params[:num1].to_i
- #   @num2 = params[:num2].to_i
- #   @num3 = @num1 * @num2
- #   "#{@num3}"
- # end
 end
